b46b49db9fa8cdf1590980018fa543c8fd3f65f4
Abhishek Kumar <Nain95552@gmail.com>
2026-02-19 16:06:43 +0530

    Update DATABASE_URI with new credentials

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aka528140:hG20SAJq3iBoeq9l@cluster0.cuci0yg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sumitjk377_db_user:abhi7860@cluster0.gthhfxt.mongodb.net/?appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://ckck44484:FsLjWzelbSk9rg17@cluster0.mducxwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Abhi12")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
