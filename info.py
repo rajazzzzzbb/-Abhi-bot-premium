81847afef16071a574ffb4107fde9dfed91d226c
Abhishek Kumar <Nain95552@gmail.com>
2026-02-21 10:10:48 +0530

    Fix environment variable retrieval for STREAM_SITE

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ CODE = (environ.get('CODE', 'https://t.me/movies_updet'))
 
 #stream link shortner
 Stream link shortener config
-STREAM_SITE = os.environ.get('STREAM_SITE', 'http://Liteshort.com')
+STREAM_SITE = (environ.get('STREAM_SITE', 'http://Liteshort.com'))
 STREAM_API = (environ.get('STREAM_API', '094cac42e1c39c257f8f1cd79faed60098fc83b2')
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/+GK5SsPo9cWVjMzI1'))
 
